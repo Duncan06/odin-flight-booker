7a89e90f783a9b1841462dbae7cae57694fa5283
Rumsauce <tylerdownie0@gmail.com>
2021-03-05 13:24:47 -0800

    Work on how to remove duplicate dates despite time variations with datetime

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,3 +1,12 @@
 class Flight < ApplicationRecord
   belongs_to :airport
+
+  def flight_date_formatted
+    scheduled.strftime("%m/%d/%Y")
+  end
+
+  def self.get_scheduled_dates
+      scheduled_dates = Flight.find_by_sql("select distinct scheduled from flights where scheduled is not null order by scheduled")
+  end
+
 end
