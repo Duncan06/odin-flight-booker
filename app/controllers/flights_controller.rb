2c8ded82d4b1b73c8a70e949941b84d386413f4f
Rumsauce <tylerdownie0@gmail.com>
2021-03-09 01:10:19 -0800

    Add search to controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,4 +7,9 @@ class FlightsController < ApplicationController
     def show
     end
 
+    def search
+        vars = request.query_parameters
+        @flights = Flight.where("start = ? AND end = ? AND scheduled = ?", vars[from_code], vars[to_code], vars[date])
+    end
+
 end
