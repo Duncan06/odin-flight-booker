7e2f8e3f7a095c60b3d735b5f3291c30edb176ad
Rumsauce <tylerdownie0@gmail.com>
2021-03-10 13:41:38 -0800

    Start work on organizing flights by dates only for dropdown

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,11 +2,16 @@ class Flight < ApplicationRecord
   belongs_to :airport
 
   def flight_date_formatted
-    scheduled.strftime("%m/%d/%Y %H:%M")
+    scheduled.strftime("%B %d, %Y")
   end
 
   def self.get_scheduled_dates
-      scheduled_dates = Flight.find_by_sql("select scheduled from flights where scheduled is not null order by scheduled")
+      scheduled_dates = Flight.find_by_sql("select distinct scheduled from flights where scheduled is not null order by scheduled")
+      dates = []
+      scheduled_dates.each do |flight|
+        dates << flight.scheduled.to_date
+      end
+      dates = dates.uniq
   end
 
 end
