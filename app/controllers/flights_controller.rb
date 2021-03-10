23c705aeee0c070318ca106b4b312046a0fdf0f3
Rumsauce <tylerdownie0@gmail.com>
2021-03-10 01:40:35 -0800

    Rework controller to have index do search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,11 @@
 class FlightsController < ApplicationController
 
     def index
-        @flights = Flight.all
+        vars = request.query_parameters
+        @flights = Flight.where("start = ? AND end = ? AND scheduled = ?", vars[:from_code], vars[:to_code], vars[:date])
     end
 
     def show
-    end
-
-    def search
-        vars = request.query_parameters
-        @flights = Flight.where("start = ? AND end = ? AND scheduled = ?", vars[from_code], vars[to_code], vars[date])
     end
 
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,11 +2,11 @@ class Flight < ApplicationRecord
   belongs_to :airport
 
   def flight_date_formatted
-    scheduled.strftime("%m/%d/%Y %I:%M%p")
+    scheduled.strftime("%m/%d/%Y %H:%M")
   end
 
   def self.get_scheduled_dates
-      scheduled_dates = Flight.find_by_sql("select distinct scheduled from flights where scheduled is not null order by scheduled")
+      scheduled_dates = Flight.find_by_sql("select scheduled from flights where scheduled is not null order by scheduled")
   end
 
 end
