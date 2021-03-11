54dd4c45fb6dcb7b88f6a34d4ca29e0292d7221e
Rumsauce <tylerdownie0@gmail.com>
2021-03-10 21:00:10 -0800

    Correct dropdown for dates on index

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,15 +1,11 @@
 class Flight < ApplicationRecord
   belongs_to :airport
 
-  def flight_date_formatted
-    scheduled.strftime("%B %d, %Y")
-  end
-
   def self.get_scheduled_dates
       scheduled_dates = Flight.find_by_sql("select distinct scheduled from flights where scheduled is not null order by scheduled")
       dates = []
       scheduled_dates.each do |flight|
-        dates << flight.scheduled.to_date
+        dates << flight.scheduled.strftime("%m/%d/%Y")
       end
       dates = dates.uniq
   end
