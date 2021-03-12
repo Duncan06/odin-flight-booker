1efca966c23a5e511821edbcc7be2e9f0b1a57f2
Rumsauce <tylerdownie0@gmail.com>
2021-03-11 23:07:35 -0800

    Add proper listing of flights by specified dates

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,21 @@ class FlightsController < ApplicationController
 
     def index
         vars = request.query_parameters
-        @flights = Flight.where("start = ? AND end = ?", vars[:from_code], vars[:to_code])
+        matches = Flight.where("start = ? AND end = ?", vars[:from_code], vars[:to_code])
+        date_matches = []
+        matches.each do |flight|
+            date_matches << [flight.id, flight.scheduled.strftime("%m-%d-%Y")]
+        end
+        id_matches = []
+        date_matches.each do |dates|
+            if dates[1] == vars[:date]
+                id_matches << dates[0]
+            end
+        end
+        @flights = []
+        id_matches.each do |match|
+            @flights << Flight.find(match)
+        end
     end
 
     def show
