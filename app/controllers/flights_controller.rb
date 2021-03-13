040232898be686e273350ed2f77cd311d413f339
Rumsauce <tylerdownie0@gmail.com>
2021-03-13 14:10:12 -0800

    Add radio button and make results for search a form, add bookings controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,15 +1,15 @@
 class FlightsController < ApplicationController
 
     def index
-        vars = request.query_parameters
-        matches = Flight.where("start = ? AND end = ?", vars[:from_code], vars[:to_code])
+        @vars = request.query_parameters
+        matches = Flight.where("start = ? AND end = ?", @vars[:from_code], @vars[:to_code])
         date_matches = []
         matches.each do |flight|
             date_matches << [flight.id, flight.scheduled.strftime("%m-%d-%Y")]
         end
         id_matches = []
         date_matches.each do |dates|
-            if dates[1] == vars[:date]
+            if dates[1] == @vars[:date]
                 id_matches << dates[0]
             end
         end
