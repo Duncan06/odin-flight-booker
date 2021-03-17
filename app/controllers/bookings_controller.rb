199eaa1923515901cbc9fe945b8b9886f71277f9
Rumsauce <tylerdownie0@gmail.com>
2021-03-16 23:42:40 -0700

    Setup new booking in controller and view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,13 @@
 class BookingsController < ApplicationController
+
+    def show
+    end
+
+    def new
+        @info = request.query_parameters
+        @booking = Booking.new
+    end
+    
+    def create
+    end
 end
