3dbc22c8e24ee7c4bf32d9315201e2b431996757
Rumsauce <tylerdownie0@gmail.com>
2021-03-16 23:51:11 -0700

    Add passenger build for fields_for implementation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,6 +6,7 @@ class BookingsController < ApplicationController
     def new
         @info = request.query_parameters
         @booking = Booking.new
+        @info[:num_tickets].times { @booking.passenger.build }
     end
     
     def create
